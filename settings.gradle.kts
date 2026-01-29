0f989892bf318cdaf90e8d27d8e1f5a6b024df82
Masahiro Kitagawa <arctica0316@gmail.com>
2026-01-29 21:37:21 +0900

    fix(gradle): Unsupported Kotlin plugin version
    The `embedded-kotlin` and `kotlin-dsl` plugins rely on features of Kotlin `2.2.20` that might work differently than in the requested version `2.2.0`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.os.OperatingSystem
 
 plugins {
-    kotlin("jvm") version "2.2.0" apply false
+    kotlin("jvm") version "2.2.20" apply false
     id("org.beryx.runtime") version "1.13.1" apply false
 }
 
